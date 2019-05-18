a62e2ad2b65dcf82a064b4a123c09d2c69300c48
Billy.Zheng <vil963@gmail.com>
2019-05-18 22:05:43 +0800

    install mtr install to arch linux.

--- init1.sh
+++ init1.sh
@@ -89,13 +89,16 @@ pacman -Sy
 # grub-mkconfig -o /boot/grub/grub.cfg
 
 # 网络相关工具
-ins ntp
-ins openssh
-ins wget
-ins netcat
-ins traceroute
-ins bind-tools
-ins rsync
+
+# mtr工具的主要作用是在于两点丢包时候的异常点排查及路径搜集，是ping和tracert的结合。
+# 相比于ping它会有路由节点的展示，而相对于tracert它会展示中间路由节点的丢包情况，
+# 可以根据丢包梯度情况简单分析出可能的异常节点并向对应运营商进行反馈。
+
+# 由于骨干外网路径可能存在的异步路由（即数据包来回路径不一致，可能在某一方向看无明显异常点，
+# 另一方向才会显示异常）与ECMP（运营商在多根路径上做负载均衡，某一根异常导致部分IP丢包），
+# 建议提供双向mtr。
+
+ins ntp openssh wget netcat traceroute bind-tools rsync mtr
 
 ins bash-completion
 
@@ -105,6 +108,9 @@ systemctl enable cronie
 ins nfs-utils
 systemctl enable nfs-server
 systemctl enable rpcbind
+
+# 其他工具
+ins lsof
 
 # # 最新的 Gnome3 不含 xorg 的, 使用的是 Wayland.
 # ins xorg
