689890c99a09ba53238066568ee79fa5ffbc53c0
Billy.Zheng <vil963@gmail.com>
2021-09-23 15:10:07 +0800

    Update

--- arch_sound_init.sh
+++ arch_sound_init.sh
@@ -1,9 +1,8 @@
 
-
 # 安装 高级Linux声音架构（Advanced Linux Sound Architecture，简称ALSA）
 # 它替换了原有的开放声音系统(OSS)
 # 如果不工作, 安装 alsa-oss
-pacman -S alsa-utils pavucontrol
+pacman -S alsa-utils pavucontrol alsa-oss
 # 运行 alsamixer,  M 取消静音, 调整音量为合适大小.
 alsamixer
 # 测试声音, 会创建 /etc/asound.state 保存设置.
